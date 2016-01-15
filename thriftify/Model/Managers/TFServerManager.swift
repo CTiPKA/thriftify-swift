1bd49625dad15f30bfb62ab7f2f4e9518681196b
Vadim Trulyaev <trulyaev@gmail.com>
2016-01-16 00:29:22 +0300

    Added function to get all transactions to server manager.

--- thriftify/Model/Managers/TFServerManager.swift
+++ thriftify/Model/Managers/TFServerManager.swift
@@ -9,6 +9,99 @@
 import UIKit
 import Alamofire
 
-class TFServerManager: NSObject {
+class TFServerManager {
+    
+    let parseHeaders : [String:String]
+    
+    init() {
+        parseHeaders = [
+            "Content-Type": "application/json",
+            "X-Parse-Application-Id": "XDh0czeU4HSzoWCC5R6Bhv8MRuHVQxL4Cvfv9FOQ",
+            "X-Parse-REST-API-Key": "cgH2dajw4usnlR23UT4jOjXqWuhDSwONQQ6WysNg"
+        ]
+    }
+    
+    func getPictures () {
+        
+//        let headers = [
+//            "Content-Type": "application/json",
+//            "X-Parse-Application-Id": "XDh0czeU4HSzoWCC5R6Bhv8MRuHVQxL4Cvfv9FOQ",
+//            "X-Parse-REST-API-Key": "cgH2dajw4usnlR23UT4jOjXqWuhDSwONQQ6WysNg"
+//        ]
+        
+        Alamofire.request(.GET, "https://api.parse.com/1/classes/Transactions", headers: parseHeaders)
+            .responseJSON() {
+                response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+//                print(response.data)     // server data
+//                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+    }
+    
+    func getTransactions ()
+    {
+        Alamofire.request(TFServerManager.Router.TransactionListFull)
+            .responseJSON () {
+                response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+    }
+    
+    
+    enum Router: URLRequestConvertible {
+        //Error: Type 'Five100px.Router' does not conform to protocol 'URLRequestConvertible'
+        
+        static let baseURLString = "https://api.parse.com/1"
+        static let consumerKey = "XDh0czeU4HSzoWCC5R6Bhv8MRuHVQxL4Cvfv9FOQ"
+        static let RESTAPIKey = "cgH2dajw4usnlR23UT4jOjXqWuhDSwONQQ6WysNg"
+        
+        case TransactionListFull
+        case PopularPhotos(Int)
+        case Comments(Int, Int)
+        
+        var URLRequest: NSMutableURLRequest {
+            
+            let (path, parameters) : (String, [String: AnyObject]?) = {
+                
+                switch self {
+                case .TransactionListFull:
+                    return ("/classes/Transactions",nil)
+                    
+                case .PopularPhotos(let page):
+                    let params = ["consumer_key": Router.consumerKey, "page": "\(page)", "feature": "popular", "rpp": "50", "include_store": "store_download", "include_status": "votes"]
+                    return ("/phtos", params)
+                    
+                case .Comments(let photoID, let commentsPage):
+                    let params = ["consumer_key": Router.consumerKey, "comments": "1", "comments_page": "\(commentsPage)"]
+                    return ("/photos/\(photoID)/comments", params)
+                }
+            }()
+            
+            let URL = NSURL(string: Router.baseURLString)
+            let URLRequest = NSMutableURLRequest(URL: URL!.URLByAppendingPathComponent(path))
+            URLRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            URLRequest.setValue(Router.consumerKey, forHTTPHeaderField: "X-Parse-Application-Id")
+            URLRequest.setValue(Router.RESTAPIKey, forHTTPHeaderField: "X-Parse-REST-API-Key")
+            
+            let encoding = Alamofire.ParameterEncoding.URL
+            
+            return encoding.encode(URLRequest, parameters: parameters).0
+        }
+    }
+}
 
+func += <KeyType, ValueType> (inout left: Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>) {
+    for (k, v) in right {
+        left.updateValue(v, forKey: k)
+    }
 }
